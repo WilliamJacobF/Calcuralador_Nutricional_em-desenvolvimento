da5773d192353323866347479170ba1d6f691f7d
WilliamJacobF <155196664+WilliamJacobF@users.noreply.github.com>
2026-04-04 21:12:48 -0300

    testes com api

--- Calculo_nutricional/resources/views/welcome.blade.php
+++ Calculo_nutricional/resources/views/welcome.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>bah</title>
+    <title>teste</title>
 </head>
 <body>
-    <h1>calculadora de nutrientes</h1>
+    teste
 </body>
 </html>
