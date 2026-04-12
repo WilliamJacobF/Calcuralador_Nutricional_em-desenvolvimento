343524ab5fb3eb019dca35c7847ba593105037bc
WilliamJacobF <155196664+WilliamJacobF@users.noreply.github.com>
2026-04-12 16:58:34 -0300

    toques finais 2

--- Calculo_nutricional/resources/views/welcome.blade.php
+++ Calculo_nutricional/resources/views/welcome.blade.php
@@ -1,85 +1,92 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-    
+
 
 <head>
-  <meta charset="UTF-8">
-  <title>nutrição</title>
-  <link rel="icon" href="apple-whole-solid-full.svg" type="image/png">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-  <style>
-    .body{
-      background-image: url("aliment.png");
+    <meta charset="UTF-8">
+    <title>nutrição</title>
+    <link rel="icon" href="apple-whole-solid-full.svg" type="image/png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <style>
+    .body {
+        background-image: url("aliment.png");
     }
-    h1,h3{
-      text-align: center;
+
+    h1,
+    h3 {
+        text-align: center;
     }
-    .corpo{
-      position: absolute;
-            top: 35%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            background: rgb(255, 255, 255);
-            padding: 20px;
-            border-radius: 15px;
-            text-align: center;
+
+    .corpo {
+        position: absolute;
+        top: 35%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background: rgb(255, 255, 255);
+        padding: 20px;
+        border-radius: 15px;
+        text-align: center;
     }
-    .testo{
-      position: absolute;
-      top: 55%;
-      padding: 20px;
+
+    .testo {
+        position: absolute;
+        top: 55%;
+        padding: 20px;
     }
-    
-  </style>
+    </style>
 </head>
+
 <body class="body">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-<br>
-<div class="head">
-    <h1>Calculadora de Nutrientes</h1>
-    <h3>Projeto de Extenção do curso de Engenharia de Software</h3>
-    <hr>
-  </div>
-<div class="corpo">
-  <form id="formBusca">
-    <label class="divlabel" for="Nome">Digite o nome do alimento:</label><br>
-    <input type="text" id="Nome" placeholder="exemplo:arroz" required><br>
-    <label for="quantidade">Quantidade (g):</label><br>
-    <input type="number" id="quantidade" placeholder="ex: 300" required><br><br>
-    <button type="submit" class="btn btn-primary" >Buscar</button>
-  </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+    </script>
+    <br>
+    <div class="head">
+        <h1>Calculadora de Nutrientes</h1>
+        <h3>Projeto de Extenção do curso de Engenharia de Software</h3>
+        <hr>
+    </div>
+    <div class="corpo">
+        <form id="formBusca">
+            <label class="divlabel" for="Nome">Digite o nome do alimento:</label><br>
+            <input type="text" id="Nome" placeholder="exemplo:arroz" required><br>
+            <label for="quantidade">Quantidade (g):</label><br>
+            <input type="number" id="quantidade" placeholder="ex: 300" required><br><br>
+            <button type="submit" class="btn btn-primary">Buscar</button>
+        </form>
 
 
-  <div class="resultado" id="resultado"></div>
+        <div class="resultado" id="resultado"></div>
 
-  
 
-  <script>
-    document.getElementById('formBusca').addEventListener('submit', function (e) {
-    e.preventDefault();
 
-    const nome = document.getElementById('Nome').value;
-    const quantidade = document.getElementById('quantidade').value;
-    const resultadoDiv = document.getElementById('resultado');
+        <script>
+        document.getElementById('formBusca').addEventListener('submit', function(e) {
+            e.preventDefault();
 
-    fetch(`http://localhost:8000/api/alimentos/buscar/${encodeURIComponent(nome)}`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('alimento não encontrado');
-        }
-        return response.json();
-      })
-      .then(data => {
+            const nome = document.getElementById('Nome').value;
+            const quantidade = document.getElementById('quantidade').value;
+            const resultadoDiv = document.getElementById('resultado');
 
-        // regra de 3 (base 100g)
-        const fator = quantidade / 100;
+            fetch(`http://localhost:8000/api/alimentos/buscar/${encodeURIComponent(nome)}`)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('alimento não encontrado');
+                    }
+                    return response.json();
+                })
+                .then(data => {
 
-        const calorias = data.Calorias * fator;
-        const proteinas = data.Proteinas * fator;
-        const carboidratos = data.Carboidratos * fator;
-        const gorduras = data.Gorduras * fator;
+                    // regra de 3 (base 100g)
+                    const fator = quantidade / 100;
 
-        resultadoDiv.innerHTML = `
+                    const calorias = data.Calorias * fator;
+                    const proteinas = data.Proteinas * fator;
+                    const carboidratos = data.Carboidratos * fator;
+                    const gorduras = data.Gorduras * fator;
+
+                    resultadoDiv.innerHTML = `
           <strong>${data.Nome}</strong><br>
           Quantidade: ${quantidade}g<br><br>
           Calorias: ${calorias.toFixed(2)}<br>
@@ -87,20 +94,23 @@
           Carboidratos: ${carboidratos.toFixed(2)}g<br>
           Gorduras: ${gorduras.toFixed(2)}g
         `;
-      })
-      .catch(error => {
-        resultadoDiv.innerHTML = error.message;
-      });
-  });
-  </script>
- 
-</div>
-<div class="testo">
-  <hr>
-  <h2>Sobre o Projeto</h2>
-  <p class="fs-4">Um site que mostra as Calorias, Proteínas, Carboidratos e Gorduras dos Alimentos buscados, feito em Laravel usando php, HTML, CSS, Bootstrap, ligado a um banco de dados relacional(mysql).</p>
-  <h2>O que é um projeto de extenção</h2>
-  <p class="fs-4">É um componente curricular obrigatória que exige que o aluno aplique os conhecimentos teóricos do seu curso em ações práticas na comunidade.</p><br>
-</div>
+                })
+                .catch(error => {
+                    resultadoDiv.innerHTML = error.message;
+                });
+        });
+        </script>
+
+    </div>
+    <div class="testo">
+        <hr>
+        <h2>Sobre o Projeto</h2>
+        <p class="fs-4">Um site que mostra as Calorias, Proteínas, Carboidratos e Gorduras dos Alimentos buscados, feito
+            em Laravel usando php, HTML, CSS, Bootstrap, ligado a um banco de dados relacional(mysql).</p>
+        <h2>O que é um projeto de extenção</h2>
+        <p class="fs-4">É um componente curricular obrigatória que exige que o aluno aplique os conhecimentos teóricos
+            do seu curso em ações práticas na comunidade.</p><br>
+    </div>
 </body>
+
 </html>
